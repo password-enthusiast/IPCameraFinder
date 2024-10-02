9375fdc11a2417dc5ef878df96b831b99dc4026a
Joshua Alberstadt <151634793+opreroma@users.noreply.github.com>
2024-10-02 19:51:43 -0400

    Update detect.bat
    capital letter where it didnt belong

--- detect.bat
+++ detect.bat
@@ -5,12 +5,11 @@ curl %targetip% > templog.txt
 
 for %%k in (%keywords%) do (
     findstr /i "%%k" templog.txt
-    if !errorlevel! equ 0 (set "foundKeyword=true")
-
+    if !errorlevel! equ 0 (set "foundkeyword=true")
 )
 
 if "!foundKeyword!" == "true" (
+    echo =============================%targetip%================================= >> permlog.txt
     type templog.txt >> permlog.txt
     nmap -p 80,554,8080,8081,1024,1026 -sV --script rtsp-url-brute,http-title -T4 %targetip% >> permlog.txt
-    echo ============================================================== >> permlog.txt
 ) 
