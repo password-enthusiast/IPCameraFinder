b5ab0c26f6f66bf163fa2c7cd43498b9154ff3d8
J.A. <151634793+password-enthusiast@users.noreply.github.com>
2024-10-24 21:20:05 -0400

    Update detect.bat
    curl silently for better speeed (i sure hope)

--- detect.bat
+++ detect.bat
@@ -1,6 +1,6 @@
 set "foundkeyword=false"
 
-curl %targetip% > templog.txt
+curl -s %targetip% > templog.txt
 
 for %%k in (%camstreamkeywords%) do (
     findstr /i "%%k" templog.txt
@@ -10,5 +10,4 @@ for %%k in (%camstreamkeywords%) do (
 if "!foundKeyword!" == "true" (
     echo =============================%targetip%================================= >> permlog.txt
     type templog.txt >> permlog.txt
-    nmap -p 80,554,8080,8081,1024,1026 --script rtsp-url-brute,http-title -T4 %targetip% >> permlog.txt
 ) 
